1392b1f6588744725aae496c546223909c5bf222
Gustavo Santiago <gsantiago@telconet.ec>
2023-09-18 17:20:47 -0500

    Added not found route

--- src/app.ts
+++ src/app.ts
@@ -30,6 +30,8 @@ app.use("/*", jwt({ secret: JWT_SECRET, cookie: "token" }));
 app.get("/api/v1/content-manager/products", getProducts);
 app.get("/api/v1/content-manager/products/:id", getProduct);
 
+// 404
+app.get("*", (c) => c.notFound());
 app.onError(handleErrors);
 logger.info(`Running at http://localhost:${port}`);
 
