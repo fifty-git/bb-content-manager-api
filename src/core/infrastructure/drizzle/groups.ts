45fd04b78a37a5d8152ea57792b3f911bc55ab1d
Gustavo Santiago <gsantiago@telconet.ec>
2023-09-19 14:26:39 -0500

    Added getProduct by ID

--- src/core/infrastructure/drizzle/groups.ts
+++ src/core/infrastructure/drizzle/groups.ts
@@ -6,7 +6,7 @@ import { product_group_link } from "~/schema/products";
 export class GroupsDS {
   static async getGroupByProductID(product_id: number) {
     return db
-      .select({ group_id: groups.group_id, name: groups.name, parent: groups.parent_group_id })
+      .select({ group_id: groups.group_id, name: groups.name })
       .from(groups)
       .innerJoin(product_group_link, eq(product_group_link.group_id, groups.group_id))
       .where(and(eq(product_group_link.product_id, product_id), isNull(groups.parent_group_id)))
@@ -16,7 +16,7 @@ export class GroupsDS {
 
   static async getSubgroupByProductID(product_id: number) {
     return db
-      .select({ group_id: groups.group_id, name: groups.name, parent: groups.parent_group_id })
+      .select({ group_id: groups.group_id, name: groups.name, parent_group_id: groups.parent_group_id })
       .from(groups)
       .innerJoin(product_group_link, eq(product_group_link.group_id, groups.group_id))
       .where(and(eq(product_group_link.product_id, product_id), isNotNull(groups.parent_group_id)))
