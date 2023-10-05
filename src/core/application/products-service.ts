d8b3d535eab35a0963c3c76bb71840a1f3245d2d
Gustavo Santiago <gsantiago@telconet.ec>
2023-10-05 11:53:56 -0500

    Modified product table to allow for bundles too

--- src/core/application/products-service.ts
+++ src/core/application/products-service.ts
@@ -83,5 +83,5 @@ export async function deleteProduct(c: Context<EnvAPI>) {
     await ProductsDS.delete(+id);
   });
 
-  return c.json({ status: "success", msg: `Product ${id} was deleted successfully` });
+  return c.json(null, 204);
 }
