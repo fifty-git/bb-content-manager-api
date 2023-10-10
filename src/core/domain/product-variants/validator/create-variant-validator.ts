c8598327ba77a978b3115a556a003a016d633dba
Gustavo Santiago <gsantiago@telconet.ec>
2023-10-10 09:47:25 -0500

    Added trim to string zod validation

--- src/core/domain/product-variants/validator/create-variant-validator.ts
+++ src/core/domain/product-variants/validator/create-variant-validator.ts
@@ -3,12 +3,12 @@ import { PRICE_REGEX } from "~/core/domain/constants";
 
 export const CreateVariantAPISchema = z.object({
   product_id: z.number().min(1, "Product ID is invalid"),
-  name: z.string(),
+  name: z.string().trim().nonempty("Variant name is required and cannot be empty"),
   measure_units: z.string().nonempty("Measure unit is required and cannot be empty"),
   units: z.number().refine((value) => value > 0, "Units must be greater than zero"),
   price: z
     .string()
     .refine((price) => PRICE_REGEX.test(price), "Invalid price format. Price should have up to two decimal places and no currency symbols")
     .refine((price) => parseFloat(price) > 0, "Price must be greater than zero"),
-  upc: z.string(),
+  upc: z.string().trim(),
 });
--- src/core/domain/products/validator/create-product-validator.ts
+++ src/core/domain/products/validator/create-product-validator.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const NewProductSchema = z.object({
-  name: z.string().nonempty("Product name must not be empty"),
+  name: z.string().trim().nonempty("Product name is required and cannot be empty"),
   group_id: z.number().optional(),
   subgroup_id: z.number().optional(),
   is_standalone: z.boolean(),
