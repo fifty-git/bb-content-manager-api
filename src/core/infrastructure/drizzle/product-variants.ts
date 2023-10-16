36ea9f2b5f63d655679e8fc146b140dc7dd28b52
Gustavo Santiago <gsantiago@telconet.ec>
2023-10-16 17:36:54 -0500

    Fixed json response for product variant activation

--- src/core/infrastructure/drizzle/product-variants.ts
+++ src/core/infrastructure/drizzle/product-variants.ts
@@ -10,7 +10,7 @@ export class ProductVariantsDS {
       .select()
       .from(product_variants)
       .where(eq(product_variants.product_id, product_id))
-      .orderBy(product_variants.display_order)
+      .orderBy(product_variants.display_order, product_variants.name)
       .prepare()
       .execute();
   }
