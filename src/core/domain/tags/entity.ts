e8791491e89feae7c38a8320b54eefc43d4752a1
jaguirre <john.aguirre22@gmail.com>
2023-10-19 10:36:51 -0500

    Api Rest Product Tags best code2

--- src/core/domain/tags/entity.ts
+++ src/core/domain/tags/entity.ts
@@ -2,10 +2,10 @@ export enum TagType {
   product = "product",
   order = "order",
   purchase_order = "purchase-order",
-  searchable = "searchable",
 }
 
 export interface NewTag {
   name: string;
   type: TagType;
+  searchable: boolean;
 }
