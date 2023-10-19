bd809d46067950036c05a800d73409fef3b78918
martin86ec <martin@fiftyflowers.com>
2023-10-19 13:55:30 -0500

    feat: add countries service

---
+++ src/core/infrastructure/drizzle/cities.ts
@@ -0,0 +1,17 @@
+import { eq } from "drizzle-orm";
+import { db } from "~/modules/drizzle";
+import { cities } from "~/schema/countries";
+
+export class CitiesDS {
+  static async getAllCitiesByCountryID(country_id: number) {
+    return db
+      .select({
+        city_id: cities.city_id,
+        name: cities.name,
+      })
+      .from(cities)
+      .where(eq(cities.country_id, country_id))
+      .prepare()
+      .execute();
+  }
+}
