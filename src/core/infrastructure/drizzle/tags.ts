7fe11f6f07f8249e84dace81e579a65fa8bc5e6c
jaguirre <john.aguirre22@gmail.com>
2023-11-15 09:29:28 -0500

    added validation create product tags

--- src/core/infrastructure/drizzle/tags.ts
+++ src/core/infrastructure/drizzle/tags.ts
@@ -1,9 +1,20 @@
 import type { NewTag } from "~/core/domain/tags/entity";
 import type { Transaction } from "~/core/domain/types";
+import { like } from "drizzle-orm";
 import { db } from "~/modules/drizzle";
 import { tags } from "~/schema/tags";
 
 export class TagsDS {
+  static async findByName(name: string, limit = 10) {
+    return db
+      .select()
+      .from(tags)
+      .where(like(tags.name, `%${name}%`))
+      .limit(limit)
+      .prepare()
+      .execute();
+  }
+
   static async create(data: NewTag, tx?: Transaction) {
     return (tx ?? db).insert(tags).values(data).prepare().execute();
   }
