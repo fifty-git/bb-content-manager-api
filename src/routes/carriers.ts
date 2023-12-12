8381b7fa436a77b4d27241baacb5862c39253608
martin86ec <martin@fiftyflowers.com>
2023-12-12 17:40:12 -0500

    feat: add carrier accounts endpoints

--- src/routes/carriers.ts
+++ src/routes/carriers.ts
@@ -1,11 +1,15 @@
 import { Hono } from "hono";
 import {
+    activateAccount,
   activateCarrier,
   activateService,
+  createAccount,
   createCarrier,
   createService,
+  deactivateAccount,
   deactivateCarrier,
   deactivateService,
+  deleteAccount,
   deleteCarrier,
   deleteService,
   getAllAccounts,
@@ -13,6 +17,7 @@ import {
   getAllCarrierServices,
   getCarrierById,
   getCarrierServicesById,
+  updateAccount,
   updateCarrier,
   updateService,
 } from "~/core/application/carriers-service";
@@ -25,6 +30,7 @@ carriersRouter.get("/:carrier_id", getCarrierById);
 carriersRouter.get("/:carrier_id/services", getAllCarrierServices);
 carriersRouter.get("/:carrier_id/services/:service_id", getCarrierServicesById);
 carriersRouter.post("/", createCarrier);
+carriersRouter.post("/accounts", createAccount);
 carriersRouter.post("/:carrier_id/services", createService);
 carriersRouter.put("/:carrier_id", updateCarrier);
 carriersRouter.put("/:carrier_id/services/:service_id", updateService);
@@ -32,5 +38,11 @@ carriersRouter.put("/:carrier_id/activate", activateCarrier);
 carriersRouter.put("/:carrier_id/deactivate", deactivateCarrier);
 carriersRouter.put("/:carrier_id/services/:service_id/activate", activateService);
 carriersRouter.put("/:carrier_id/services/:service_id/deactivate", deactivateService);
+carriersRouter.put("/accounts/:account_id", updateAccount);
+carriersRouter.put("/accounts/:account_id/activate", activateAccount);
+carriersRouter.put("/accounts/:account_id/deactivate", deactivateAccount);
+carriersRouter.post("/:carrier_id/services", createService);
 carriersRouter.delete("/:carrier_id", deleteCarrier);
 carriersRouter.delete("/:carrier_id/services/:service_id", deleteService);
+carriersRouter.delete("/accounts/:account_id", deleteAccount);
+carriersRouter.post("/:carrier_id/services", createService);
