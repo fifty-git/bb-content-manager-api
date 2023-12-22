5312df417f4dbd9863bc5bfa6f7f85dbe21e4756
martin86ec <martin@fiftyflowers.com>
2023-12-22 09:04:38 -0500

    fix: remove "account_" prefix from name and number fix: add pickup and transit days to origins object on update service

--- src/core/domain/carrier-service-accounts/validator/updateAccount.ts
+++ src/core/domain/carrier-service-accounts/validator/updateAccount.ts
@@ -2,6 +2,6 @@ import { z } from "zod";
 
 export const UpdateAccountSchema = z.object({
   account_id: z.number(),
-  account_name: z.string().trim().nonempty().optional(),
-  account_number: z.string().trim().nonempty().optional(),
+  name: z.string().trim().nonempty().optional(),
+  number: z.string().trim().nonempty().optional(),
 });
