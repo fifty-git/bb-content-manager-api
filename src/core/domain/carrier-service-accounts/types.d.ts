1bc19787a45b4e07afddf2bec564fc3027a22014
martin86ec <martin@fiftyflowers.com>
2023-12-22 09:03:56 -0500

    fix: remove account_ prefix from name and number

--- src/core/domain/carrier-service-accounts/types.d.ts
+++ src/core/domain/carrier-service-accounts/types.d.ts
@@ -2,14 +2,14 @@ import type { Status } from "../carriers/entity";
 
 export interface CarrierServiceAccount {
   account_id: number;
-  account_name: string;
-  account_number: string;
+  name: string;
+  number: string;
   status: Status;
 }
 
 export interface UpdateCarrierServiceAccount {
   account_id: number;
-  account_name?: string;
-  account_number?: string;
+  name?: string;
+  number?: string;
   status?: Status;
 }
