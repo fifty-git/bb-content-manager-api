6f72e61b0982c052c04b8e584431d586ffbcc4e7
Gustavo Santiago <gustavo@fiftyflowers.com>
2024-01-03 23:15:38 -0500

    Added a new folder structure to have better control flow of what other devs do

--- src/core/application/use-cases/base.ts
+++ src/core/application/use-cases/base.ts
@@ -9,9 +9,9 @@ export abstract class BaseUseCase {
   protected abstract process(): Promise<void>;
 
   async run(c: Context<EnvAPI>) {
-    c.var.log.info(`Hit ${c.req.url} [${c.req.method}]`);
+    c.var.log.info(`[${c.req.method}] ${c.req.url}`);
     const _data = await this.getData(c);
-    c.var.log.info(`Received: ${_data}`);
+    c.var.log.info(`Received:`, _data);
     const error = await this.validate(_data);
     if (error) return c.json({ error }, 400);
     await this.process();
