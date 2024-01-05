4f193997743da0d6105590511345a52fb546a379
Gustavo Santiago <gsantiago@telconet.ec>
2024-01-05 15:25:38 -0500

    Adding interface for use cases and data access for groups

--- src/core/application/use-cases/base.ts
+++ src/core/application/use-cases/base.ts
@@ -10,8 +10,9 @@ export abstract class BaseUseCase {
   async run(c: Context<EnvAPI>) {
     // Getting and validating the data
     c.var.log.info(`[${c.req.method}] ${c.req.url}`);
-    const data = await c.req.text();
-    if (data) c.var.log.info(data);
+    // Only for debug
+    // const data = await c.req.json();
+    // if (data) c.var.log.info(data);
     const error = await this.validateData(c);
     if (error) {
       c.var.log.error({ status: "error", msg: error });
